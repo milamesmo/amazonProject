9ce64c5237e96d8a2170235912055efc30efc9eb
milamesmo <camilaabegg.n@gmail.com>
2026-06-19 16:34:52 -0300

    Create class version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,7 +2,7 @@ import { renderOrderSumary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 import { calculateCartQuantity } from "../data/cart.js";
-import '../data/cart-oop.js';
+import '../data/cart-class.js';
 
 const cartQuantity = calculateCartQuantity();
 
