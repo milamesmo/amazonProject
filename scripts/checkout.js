57950887c76acd099ff9ee0ef856ef813d586e90
milamesmo <camilaabegg.n@gmail.com>
2026-06-25 17:47:06 -0300

    feat: add backend practice file and integrate into checkout

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,6 +3,7 @@ import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 import { calculateCartQuantity } from "../data/cart.js";
 // import '../data/cart-class.js';
+import '../data/backend-practice.js';
 
 const cartQuantity = calculateCartQuantity();
 
