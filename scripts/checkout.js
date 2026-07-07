83a8faaa097898d65e877d3b2946f12a84ecc683
milamesmo <camilaabegg.n@gmail.com>
2026-07-07 16:01:09 -0300

    feat: update product loading logic and add Appliance class for better product categorization

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,6 +3,7 @@ import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 import { calculateCartQuantity } from "../data/cart.js";
 import { loadProducts } from "../data/products.js";
+import '../data/car.js';
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js';
 
