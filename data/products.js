f65172d3f995ab0b11dd08ead3accadfe2302be8
milamesmo <camilaabegg.n@gmail.com>
2026-07-16 17:02:45 -0300

    Convert loadProducts from XHR to fetch

--- data/products.js
+++ data/products.js
@@ -82,35 +82,49 @@ export class Appliance extends Product {
   }
 }
 
+function createProduct(productDetails) {
+  const productName = productDetails.name.toLowerCase();
+
+  if (
+    productName.includes("toaster") ||
+    productName.includes("kettle") ||
+    productName.includes("blender")
+  ) {
+    productDetails.type = "appliance";
+    productDetails.instructionsLink =
+      "https://supersimple.dev/images/appliance-instructions.png";
+    productDetails.warrantyLink =
+      "https://supersimple.dev/images/appliance-warranty.png";
+  }
+
+  if (productDetails.type === "clothing") {
+    return new Clothing(productDetails);
+  }
+
+  if (productDetails.type === "appliance") {
+    return new Appliance(productDetails);
+  }
+
+  return new Product(productDetails);
+}
+
+export function loadProductsFetch() {
+  return fetch("https://supersimplebackend.dev/products")
+    .then((response) => {
+      return response.json();
+    })
+    .then((productsData) => {
+      products = productsData.map(createProduct);
+
+      console.log("load products");
+    });
+}
+
 export function loadProducts(fun) {
   const xhr = new XMLHttpRequest();
 
   xhr.addEventListener("load", () => {
-    products = JSON.parse(xhr.response).map((productDetails) => {
-      const productName = productDetails.name.toLowerCase();
-
-      if (
-        productName.includes("toaster") ||
-        productName.includes("kettle") ||
-        productName.includes("blender")
-      ) {
-        productDetails.type = "appliance";
-        productDetails.instructionsLink =
-          "https://supersimple.dev/images/appliance-instructions.png";
-        productDetails.warrantyLink =
-          "https://supersimple.dev/images/appliance-warranty.png";
-      }
-
-      if (productDetails.type === "clothing") {
-        return new Clothing(productDetails);
-      }
-
-      if (productDetails.type === "appliance") {
-        return new Appliance(productDetails);
-      }
-
-      return new Product(productDetails);
-    });
+    products = JSON.parse(xhr.response).map(createProduct);
 
     console.log("load products");
 
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,28 +1,24 @@
 import { renderOrderSumary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
-import { calculateCartQuantity } from "../data/cart.js";
-import { loadProducts } from "../data/products.js";
-import { loadCart } from "../data/cart.js";
+import { calculateCartQuantity, loadCart } from "../data/cart.js";
+import { loadProductsFetch } from "../data/products.js";
 
-// import '../data/cart-class.js';
-// import '../data/backend-practice.js';
+// import "../data/cart-class.js";
+// import "../data/backend-practice.js";
 
 Promise.all([
-new Promise((resolve) => {
-loadProducts(() =>{
-    const cartQuantity = calculateCartQuantity();
-    renderCheckoutHeader(cartQuantity);
-    resolve();
-});
-}),
-new Promise ((resolve)=>{
-        loadCart(() =>{
-            resolve();
-        });
-    })
-]).then(() =>{
-    renderOrderSumary();
-    renderPaymentSummary();   
-});
+  loadProductsFetch(),
 
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  }),
+]).then(() => {
+  const cartQuantity = calculateCartQuantity();
+
+  renderCheckoutHeader(cartQuantity);
+  renderOrderSumary();
+  renderPaymentSummary();
+});
